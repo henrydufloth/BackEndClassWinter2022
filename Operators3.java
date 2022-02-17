1edfb66f289ab4bd2e8fa53ba6e7acf477be4d3c
henrydufloth <henrydufloth@outlook.com>
2022-02-16 21:04:43 -0600

    Create Operators3.java

---
+++ Operators3.java
@@ -0,0 +1,21 @@
+package Package;
+
+public class Operators3 {
+	public static void main(String[] args) {
+		// + - / *
+		
+		int age = 23;
+		System.out.println(age);
+		
+		boolean isAge = age == 30;
+		System.out.println(isAge);
+		
+		age = age * 3;
+		System.out.println(age);
+		
+		String firstName = "Henry";
+		String lastName = "Dufloth";
+		String fullName = firstName + " " + lastName; //concatenation
+		System.out.println(fullName);
+	}
+}
